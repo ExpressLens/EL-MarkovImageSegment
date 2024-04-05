7106469160c458b1c92a6aca846d277e122f645a
ExpressLens <expresslens@proton.me>
2024-04-05 09:00:15 +0000

    ext4: don't save the error information if the block device is read-only

--- src/config.h
+++ src/config.h
@@ -19,4 +19,5 @@ public:
     // Eau, fond, fond relief, bat1, bat2, rouge vif, zone, texte, trait fin
     static cv::Vec3b colors[NB_COLORS];
     static constexpr double beta = 1.5;
-    static constexpr double initial_tempera
+    static constexpr double initial_temperature = 10.0;
+  
