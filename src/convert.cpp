edf5643a4cc1e6f2d074a6b67fa8677f179eadce
ExpressLens <expresslens@proton.me>
2024-04-17 23:08:43 +0000

    Update stats.md

--- src/convert.cpp
+++ src/convert.cpp
@@ -6,3 +6,15 @@
 //  Created by ixi on 17/12/14.
 //
 //
+
+#include "convert.h"
+
+void convert(cv::Mat& prob, cv::Mat& output)
+{
+    for (int i = 0; i < prob.rows; ++i)
+    {
+        for (int j = 0; j < prob.cols; ++j)
+        {
+            output.at<cv::Vec3b>(i, j) = Config::colors[prob.at<uchar>(i, j)];
+        }
+    }
