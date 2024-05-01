77f19d18f4fe7f097fc8b6a088f4666d59f92024
ExpressLens <expresslens@proton.me>
2024-05-01 20:45:53 +0000

    2.2.3

--- src/cost.cpp
+++ src/cost.cpp
@@ -27,3 +27,19 @@ void Cost::compute_mean_variance_(cv::Mat& image, int classe)
     {
         sum = 0;
         sum2 = 0;
+
+        for (int i = 0; i < image.rows; ++i)
+        {
+            for (int j = 0; j < image.cols; ++j)
+            {
+                sum += image.at<cv::Vec3b>(i, j)[k];
+                sum2 += image.at<cv::Vec3b>(i, j)[k] *
+                        image.at<cv::Vec3b>(i, j)[k];
+            }
+        }
+
+        mean_[classe](k) = sum / (image.cols * image.rows);
+        covariance_[classe](k, k) = (sum2 - (sum * sum) / (image.cols * image.rows))
+                                    / (image.cols * image.rows);
+    }
+}
