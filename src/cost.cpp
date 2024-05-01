79a576d8b10f8247914ba8176bf45f893085e423
ExpressLens <expresslens@proton.me>
2024-05-01 16:18:35 +0000

    godep

--- src/cost.cpp
+++ src/cost.cpp
@@ -10,3 +10,12 @@
 #include "cost.h"
 
 Cost::Cost()
+{
+    for (int i = 0; i < NB_COLORS; ++i)
+    {
+        covariance_[i].zeros();
+        inv_covariance_[i].zeros();
+    }
+}
+
+void Cost::compute_mean_variance_(cv::Mat& image, int classe)
