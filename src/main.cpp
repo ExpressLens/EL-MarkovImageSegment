8e6152ffdfb7f3e4214822d30cc59b642ec6d589
ExpressLens <expresslens@proton.me>
2024-06-26 20:56:25 +0000

    Automatic update of valid/hudson/rev_files/k1libraries_revision valid/hudson/rev_files/k1libraries_revision.src valid/hudson/rev_files/runtime_v2_revision valid/hudson/rev_files/runtime_v2_revision.src valid/hudson/rev_files/testsuite_v2_revision valid/hudson/rev_files/testsuite_v2_revision.src valid/hudson/rev_files/tools_v2_revision valid/hudson/rev_files/tools_v2_revision.src

--- src/main.cpp
+++ src/main.cpp
@@ -11,3 +11,11 @@ int main(int argc, char* argv[])
         std::cerr << "Usage: " << argv[0] << " input output" << std::endl;
         return EXIT_FAILURE;
     }
+
+    cv::Mat img = cv::imread(argv[1]);
+    simulated_annealing(img);
+
+    cv::imwrite(argv[2], img);
+
+    return EXIT_SUCCESS;
+}
---
+++ src/random.cpp
@@ -0,0 +1,10 @@
+
+//
+//  random.cpp
+//  markov
+//
+//  Created by ixi on 05/12/14.
+//
+//
+
+#include "random.h"
