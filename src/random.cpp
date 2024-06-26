ff46e38a11e8905eaf9eedea47949facc24f1170
ExpressLens <expresslens@proton.me>
2024-06-26 21:36:31 +0000

    namespace fixes

--- src/random.cpp
+++ src/random.cpp
@@ -13,3 +13,13 @@ void random_image(cv::Mat& img)
 {
     std::mt19937 eng_(time(NULL));
     std::uniform_int_distribution<uint32_t> uint_dist_;
+
+    for (int i = 0; i < img.rows; ++i)
+    {
+        for (int j = 0; j < img.cols; ++j)
+        {
+            img.at<uchar>(i, j) = uint_dist_(eng_) % NB_COLORS;
+        }
+    }
+
+}
